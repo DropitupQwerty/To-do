fa124bae8eaaf3a086d5fffe2e6d80a0fcae001e
Jacob Allen <vjacoballen3040@gmail.com>
2023-02-09 18:44:28 +0800

    todo unfinished

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+// type HeadingProps = { title: string };
+
+const Navbar = () => {
+  return (
+    <div className="bg-blue-100 p-8 shadow sm flex justify-between ">
+      <div>
+        <h1 className="font-bold color-plain">To-Do</h1>
+      </div>
+      <div>
+        <button>Add To-do</button>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
